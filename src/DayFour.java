c4c53338c22f26b3e740f82738ddf2e9b6aabdfd
Sareena Razak <11263177+sareenarazak@users.noreply.github.com>
2023-12-06 11:16:38 -0800

    Refactor Day 4
    Will revisit code structure

--- src/DayFour.java
+++ src/DayFour.java
@@ -1,42 +1,83 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.regex.*;
 
 /**
  * https://adventofcode.com/2023/day/4
  */
 public class DayFour {
+    // Example line
+    // Card   1: 24 12 26 39 19 98 74 16 82 77 | 80 11 51  1 74 60 77 68 42 35 39 78 21 12 29 19 25 98 65 91 33 17 59 24 31
+
+    public static final Pattern PATTERN = Pattern.compile("Card\\s+(\\d+):\\s+(.*)");
+
     public static void main(String[] args) {
         List<String> lines = FileUtils.readInputFile("resources/scratchcards.txt");
-        System.out.println("total points from scratch card is : " + calculateTotalPoints(lines));
+        List<Card> cards = parseLinesToCards(lines);
+        System.out.println("total points from scratch card is : " + calculateTotalPoints(cards));
     }
 
-    private static double calculateTotalPoints(List<String> lines) {
-        return lines.stream()
-                .map(DayFour::calculatePointForCard)
+    private static double calculateTotalPoints(List<Card> cards) {
+        return cards.stream()
+                .map(DayFour::calculatePoints)
                 .mapToDouble(Double::doubleValue)
                 .sum();
     }
-    private static double calculatePointForCard(String line) {
-        // Example line
-        // Card   1: 24 12 26 39 19 98 74 16 82 77 | 80 11 51  1 74 60 77 68 42 35 39 78 21 12 29 19 25 98 65 91 33 17 59 24 31
-        String allNumbers = line.split(":")[1].trim();
 
-        String[] winningAndActualNumbers  = allNumbers.split("\\|");
+    private static List<Card> parseLinesToCards(List<String> lines) {
+        List<Card> cards = new ArrayList<>();
 
-        String winningNumbers = winningAndActualNumbers[0].trim();
-        String actualNumbers = winningAndActualNumbers[1].trim();
+        for(String line : lines) {
+            Matcher matcher = PATTERN.matcher(line);
+            if (matcher.find()) {
+                String cardId = matcher.group(1);
+                String numbers = matcher.group(2);
 
-        List<Integer> winningNums = parseNumbers(winningNumbers);
-        List<Integer> actualNums = parseNumbers(actualNumbers);
+                String[] numSets = numbers.split("\\|");
+                List<Integer> winningNums = parseNumbers(numSets[0]);
+                List<Integer> actualNums = parseNumbers(numSets[1]);
 
-        long count = winningNums.stream()
+                Card card = new Card(cardId);
+                card.winningNums = winningNums;
+                card.actualNums = actualNums;
+
+                cards.add(card);
+            }
+        }
+        return cards;
+    }
+
+    private static double calculatePoints(Card card) {
+        long count = getMatchingNumberCount(card);
+        return count > 0 ? Math.pow(2, count - 1) : 0;
+    }
+
+    private static long getMatchingNumberCount(Card card) {
+        List<Integer> winningNums = card.winningNums;
+        List<Integer> actualNums = card.actualNums;
+        return winningNums.stream()
                 .filter(actualNums::contains)
                 .count();
-        return count > 0 ? Math.pow(2, count - 1) : 0L;
     }
+
     private static List<Integer> parseNumbers(String numbers) {
-        return Arrays.stream(numbers.split("\\s+"))
+        return Arrays.stream(numbers.trim().split("\\s+"))
                 .map(Integer::valueOf)
                 .toList();
+    }
+}
+
+class Card{
+    String cardId;
+    List<Integer> winningNums;
+    List<Integer> actualNums;
+
+    int count;
+
+    List<String> wonCopyCards;
+
+    public Card(String cardId) {
+        this.cardId = cardId;
+        this.winningNums = new ArrayList<>();
+        this.actualNums = new ArrayList<>();
     }
 }
