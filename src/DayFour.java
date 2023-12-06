562e07089653d3c2bf12eefc103e2e062c659a9c
Sareena Razak <11263177+sareenarazak@users.noreply.github.com>
2023-12-06 12:47:21 -0800

    Day 4 part 2

--- src/DayFour.java
+++ src/DayFour.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.regex.*;
+import java.util.stream.*;
 
 /**
  * https://adventofcode.com/2023/day/4
@@ -7,29 +8,22 @@ import java.util.regex.*;
 public class DayFour {
     // Example line
     // Card   1: 24 12 26 39 19 98 74 16 82 77 | 80 11 51  1 74 60 77 68 42 35 39 78 21 12 29 19 25 98 65 91 33 17 59 24 31
-
     public static final Pattern PATTERN = Pattern.compile("Card\\s+(\\d+):\\s+(.*)");
 
     public static void main(String[] args) {
         List<String> lines = FileUtils.readInputFile("resources/scratchcards.txt");
         List<Card> cards = parseLinesToCards(lines);
         System.out.println("total points from scratch card is : " + calculateTotalPoints(cards));
-    }
-
-    private static double calculateTotalPoints(List<Card> cards) {
-        return cards.stream()
-                .map(DayFour::calculatePoints)
-                .mapToDouble(Double::doubleValue)
-                .sum();
+        System.out.println("total points from scratch card is : " + totalWonCards(cards));
     }
 
     private static List<Card> parseLinesToCards(List<String> lines) {
         List<Card> cards = new ArrayList<>();
 
-        for(String line : lines) {
+        for (String line : lines) {
             Matcher matcher = PATTERN.matcher(line);
             if (matcher.find()) {
-                String cardId = matcher.group(1);
+                Integer cardId = Integer.parseInt(matcher.group(1));
                 String numbers = matcher.group(2);
 
                 String[] numSets = numbers.split("\\|");
@@ -46,17 +40,17 @@ public class DayFour {
         return cards;
     }
 
-    private static double calculatePoints(Card card) {
-        long count = getMatchingNumberCount(card);
-        return count > 0 ? Math.pow(2, count - 1) : 0;
+    // Part 1
+    private static double calculateTotalPoints(List<Card> cards) {
+        return cards.stream()
+                .map(DayFour::calculatePoints)
+                .mapToDouble(Double::doubleValue)
+                .sum();
     }
 
-    private static long getMatchingNumberCount(Card card) {
-        List<Integer> winningNums = card.winningNums;
-        List<Integer> actualNums = card.actualNums;
-        return winningNums.stream()
-                .filter(actualNums::contains)
-                .count();
+    private static double calculatePoints(Card card) {
+        long count =card.getMatchingNumberCount();
+        return count > 0 ? Math.pow(2, count - 1) : 0;
     }
 
     private static List<Integer> parseNumbers(String numbers) {
@@ -64,20 +58,48 @@ public class DayFour {
                 .map(Integer::valueOf)
                 .toList();
     }
+
+    // Part 2
+    // Getting part 2 to work -> to try later : store the cardIds won for each card and count using that
+    private static int totalWonCards(List<Card> cards) {
+        Map<Integer, Integer> cardCountMap = cards.stream()
+                .collect(
+                        Collectors.toMap(c -> c.cardId, c -> 1));
+
+        for (Card card : cards) {
+            int count = (int) card.getMatchingNumberCount();
+            int currentCardCount = cardCountMap.computeIfAbsent(card.cardId, c -> 1);
+
+            // For each card won add currentCardCount
+            IntStream.rangeClosed(1, count)
+                    .mapToObj(id -> card.cardId + id)
+                    .forEach(wonCardId ->
+                            cardCountMap.merge(wonCardId, currentCardCount, Integer::sum));
+        }
+
+        return cardCountMap.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
 }
 
 class Card{
-    String cardId;
+    Integer cardId;
     List<Integer> winningNums;
     List<Integer> actualNums;
 
-    int count;
-
-    List<String> wonCopyCards;
-
-    public Card(String cardId) {
+    public Card(Integer cardId) {
         this.cardId = cardId;
         this.winningNums = new ArrayList<>();
         this.actualNums = new ArrayList<>();
     }
+
+    public long getMatchingNumberCount() {
+        List<Integer> winningNums = this.winningNums;
+        List<Integer> actualNums = this.actualNums;
+        return winningNums.stream()
+                .filter(actualNums::contains)
+                .count();
+    }
+
 }
